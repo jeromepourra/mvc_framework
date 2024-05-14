565082d8b01c525247b8a02ae70891d6f1a2fa76
Jerome Pourra <j.pourra.perso@gmail.com>
2024-05-15 00:26:43 +0200

    add somes controllers

---
+++ www/controllers/HomeController.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace www\controllers;
+use core\router\Route;
+
+class HomeController
+{
+	#[Route(path: "/hello/{name}", methods: ["GET"], patterns: ['name' => "[A-Za-z-]+"])]
+	public function hello(string $name)
+	{
+		echo "HomeController::hello() -> " . $name;
+	}
+
+	#[Route(path: "/print/{firstname}/{lastname}", methods: ["GET"], patterns: ['firstname' => "[A-Za-z-]+", 'lastname' => "[A-Za-z-]+"])]
+	public function print(string $firstname, string $lastname)
+	{
+		echo "HomeController::print() -> " . $firstname . " : " . $lastname;
+	}
+}
