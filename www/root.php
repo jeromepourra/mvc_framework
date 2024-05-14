74e7c0cdc18d3d54d339b6277fd41b9846a06b6c
Jerome Pourra <j.pourra.perso@gmail.com>
2024-05-15 00:28:51 +0200

    root file register + load controller at the beginning

--- www/root.php
+++ www/root.php
@@ -1,9 +1,11 @@
 <?php
 use www\controllers\HomeController;
+use www\controllers\TestController;
 
 require "./../bootstrap.php";
 
 Router()->register(HomeController::class);
+Router()->register(TestController::class);
 Router()->load($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 
 require "./../end.php";
