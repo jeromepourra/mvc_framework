f1109f5a572eedfc02603ddbdd3823840e275fc2
Jerome Pourra <j.pourra.perso@gmail.com>
2024-05-21 19:06:44 +0200

    somes test paths in App

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -10,42 +10,24 @@ class AppTest extends TestCase
 		$this->assertInstanceOf(App::class, App());
 	}
 
-	public function testBuildRootPath(): void
-	{
-		App()->buildRootPath("base/dir");
-		$this->assertEquals("./../../", App()->mkPath());
-	}
-
-	public function testBuildRootUrl(): void
-	{
-		$_SERVER['REQUEST_URI'] = "/abc/def";
-		App()->buildRootUrl();
-		$this->assertEquals("./../../", App()->mkUrl());
-	}
-
 	public function testMkWebPath(): void
 	{
-		App()->buildRootPath("base/dir");
-		$this->assertEquals("./../../www/path/to/file", App()->mkWebPath("path/to/file"));
+		$this->assertEquals("./www/path/to/file", App()->mkWebPath("path/to/file"));
 	}
 
 	public function testMkPublicPath(): void
 	{
-		App()->buildRootPath("base/dir");
-		$this->assertEquals("./../../www/public/path/to/file", App()->mkPublicPath("path/to/file"));
+		$this->assertEquals("./www/public/path/to/file", App()->mkPublicPath("path/to/file"));
 	}
 
 	public function testMkPublicUrl(): void
 	{
-		$_SERVER['REQUEST_URI'] = "/abc/def";
-		App()->buildRootUrl();
-		$this->assertEquals("./../../public/path/to/file", App()->mkPublicUrl("path/to/file"));
+		$this->assertEquals("./public/path/to/file", App()->mkPublicUrl("path/to/file"));
 	}
 
 	public function testMkTemplatePath(): void
 	{
-		App()->buildRootPath("base/dir");
-		$this->assertEquals("./../../www/templates/path/to/file", App()->mkTemplatePath("path/to/file"));
+		$this->assertEquals("./www/templates/path/to/file", App()->mkTemplatePath("path/to/file"));
 	}
 
 }
